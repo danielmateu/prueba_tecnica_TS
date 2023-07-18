6c111fa286339a9be10ada4bbfab3937ae137f53
Daniel Mateu <danielmateu86@gmail.com>
2023-07-18 10:40:50 +0200

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import { User } from './types';
 import { UsersList } from './components/UsersList';
 import './App.css'
@@ -9,12 +10,24 @@ import './App.css'
 
 function App() {
   // Fetch data from API
+  useEffect(() => {
+    fetch('https://randomuser.me/api?results=100')
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      .then(async res => await res.json())
+      .then(res => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        setUSers(res.results)
+        originalUsers.current = res.results
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+  const originalUsers = useRef<User[]>([])
   const [users, setUSers] = useState<User[]>([])
   const [showColors, setShowColors] = useState(false)
   const [sortByCountry, setSortByCountry] = useState(false)
   const [filterCountry, setFilterCountry] = useState<string | null>(null)
   // Implement a feature that allow us to restore the initial state, meaning that all deleted rows will be recovered with useRef
-  const originalUsers = useRef<User[]>([])
 
   const toggleColors = () => {
     setShowColors(!showColors)
@@ -32,35 +45,21 @@ function App() {
     setUSers(originalUsers.current)
   }
 
-  useEffect(() => {
-    fetch('https://randomuser.me/api?results=100')
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      .then(async res => await res.json())
-      .then(res => {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-        setUSers(res.results)
-        originalUsers.current = res.results
-      })
-      .catch(err => console.log(err))
-  }, [])
-
   // Filtrar usuarios por país
-  const filteredUsers = filterCountry
-    ? users.filter((user => {
-      return user.location.country.toLowerCase().includes(filterCountry.toLowerCase())
-    }))
-    : users
+  const filteredUsers = useMemo(() => {
+    return filterCountry != null && filterCountry.length > 0
+      ? users.filter((user => {
+        return user.location.country.toLowerCase().includes(filterCountry.toLowerCase())
+      }))
+      : users
+  }, [filterCountry, users])
 
   // Ordenar usuarios sin mutar el estado
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const sortedUsers = sortByCountry
+  const sortedUsers = useMemo(() => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    ? filteredUsers.toSorted((a, b) => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
-      return a.location.country.localeCompare(b.location.country)
-    })
-    : filteredUsers
-
+    return sortByCountry ? users.toSorted((a, b) => a.location.country.localeCompare(b.location.country)) : filteredUsers
+  }, [filteredUsers, sortByCountry])
 
   return (
     <>
